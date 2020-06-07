a2eb9b5e91a3cd562e8f46621fc0c3fa6a363c68
Mark Waddoups <mwaddoups@gmail.com>
2020-06-07 11:21:07 +0100

    Added GCP data

--- prodes/plot.py
+++ prodes/plot.py
@@ -4,15 +4,15 @@ import warnings
 import matplotlib.pyplot as mpl
 from scipy.stats import percentileofscore
 
-
 def plot_deforestation(year_area):
     years = list(int(x) for x in year_area.keys())
     size = [v  for v in year_area.values()]
     N = len(size)
 
-    fig, ax = plt.subplots(figsize=(15, 10))
     warnings.warn('Increasing default font size in mpl.rcParams["font.size"] to 14...')
     mpl.rcParams['font.size'] = 14
+
+    fig, ax = plt.subplots(figsize=(15, 10))
     ax.grid(axis='y')
     ax.bar(years, size, edgecolor=(0, 0.3, 0),
            color=[(0, 0.9 - 0.7 * (percentileofscore(size, s) / 100), 0) for s in size], alpha=0.7)
@@ -23,5 +23,36 @@ def plot_deforestation(year_area):
         matplotlib.ticker.FuncFormatter(lambda x, p: f'{x:,.0f}'))
 
     ax.set_xticks(years)
+
+    plt.show()
+
+def plot_carbon_emissions(carbon_data, emissions_data):
+    mpl.rcParams['font.size'] = 14
+
+    years = list(int(x) for x in carbon_data.keys())
+    size = [v / 1e6 for v in carbon_data.values()]
+    N = len(size)
+
+    fig, ax = plt.subplots(figsize=(15, 10))
+    ax.grid(axis='y')
+    cmap = lambda s: 0.5 - 0.5 * (percentileofscore(size, s) / 100)
+    ax.bar(years, size, edgecolor='black', label='Equivalent emissions from deforestation',
+        color=[(cmap(s), cmap(s), cmap(s)) for s in size], alpha=0.7)
+
+    ax.step(
+        sorted(years), 
+        [emissions_data.get(str(y), None) for y in sorted(years)],
+        color='red',
+        linestyle='--',
+        label='Brazilian emissions from fossil fuel and industry'
+    )
+    ax.set_title('Equivalent carbon emission from deforestation')
+    ax.set_xlabel('Year')
+    ax.set_ylabel('Carbon emission equivalent (millions of tons)')
+    ax.get_yaxis().set_major_formatter(
+        matplotlib.ticker.FuncFormatter(lambda x, p: f'{x:,.0f}'))
+
+    ax.set_xticks(years)
+    ax.legend()
 
     plt.show()
--- prodes/process.py
+++ prodes/process.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from collections import defaultdict
 from dbfread import DBF
 
@@ -54,6 +55,7 @@ def process_single_year_prodes_shp_file(data_path):
 
 def process_combined_files(gfw_data, prodes_multi_year_data, single_year_data):
     """
+    Combines data from GFW and PRODES sources.
 
     :param gfw_data: path to GFW DBF file
     :param prodes_multi_year_data: path to PRODES multi-year SHP DBF file
@@ -67,3 +69,19 @@ def process_combined_files(gfw_data, prodes_multi_year_data, single_year_data):
     
     # Preference PRODES raw over GFW
     return {**gfw_data, **prodes_data, **single_years}
+
+def get_brazil_emissions_data(gcp_data):
+    """
+    Uses the data from the Global Carbon Project (2019) to extract brazilian emissions from 2000 onward
+
+    :param gcp_data: path to GCP National xls file.
+    :returns: carbon (million tons) emitted by Brazil 
+    """
+    nat_data = pd.read_excel(gcp_data, sheet_name=1, skiprows=16)
+    year = nat_data.iloc[:, 0]
+    emissions = nat_data['Brazil'] 
+
+    return {str(y): e for y, e in zip(year, emissions) if y >= 2000}
+
+def convert_deforested_to_carbon(deforested_data, basin_size_km2=7e6, carbon_in_basin=150e9):
+    return {k: v * carbon_in_basin / basin_size_km2 for k, v in deforested_data.items()}
